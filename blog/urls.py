e6d20edf617542132dec6963ad5ba9d4ce9c5117
Indrajithrnair <indrajithnair02@gmail.com>
2025-01-20 13:26:11 +0530

    Modified templates to display posts from database.

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from blog.models import Post
+from django.contrib.auth.models import User
 
 urlpatterns = [
     path('', views.post_list, name='post_list'),
